6bd1089217aa5823ed9bf1c60baa3175ebc159fa
Chris Lawrence <chris@nanimokamo.com>
2018-06-11 20:50:22 +0100

    new

--- src/components/OtherProjects/OtherProjectsItem/styles.js
+++ src/components/OtherProjects/OtherProjectsItem/styles.js
@@ -10,25 +10,12 @@ export const Arrow = styled.div`
 `;
 
 export const Main = styled.div`
-  padding: 1em 0;
+  padding: 0.5em 0;
   font-size: 20px;
   margin-left: 1.15rem;
   max-width: 700px;
-  margin-bottom: 1.5em;
-`;
-
-export const JobWrapper = styled.div`
   margin-bottom: 1em;
 `;
 
-export const Body = styled.div`
-  text-transform: initial;
-  font-weight: 400;
-  line-height: 1.4;
-  letter-spacing: 0.015em;
+export const JobWrapper = styled.div``;
 
-  a {
-    font-weight: 500;
-    border-bottom: 1px dotted currentColor;
-  }
-`;
